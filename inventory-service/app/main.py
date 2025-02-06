a1d25bc8d9817fc6842e1bf9c93e5ee7e998889d
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-02-06 07:35:57 -0500

    inventory services -- add potion and json structure

--- inventory-service/app/main.py
+++ inventory-service/app/main.py
@@ -1,16 +1,39 @@
-from fastapi import FastAPI
-from tempdb import potions_db
+from fastapi import FastAPI, HTTPException
+import json
 
 app = FastAPI()
 
+DB_FILE = "potions.json"
+
+# Load potions
+def load_potions():
+    try:
+        with open(DB_FILE, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+# Save potions
+def save_potions(potions):
+    with open(DB_FILE, "w") as file:
+        json.dump(potions, file, indent=4)
+
+
 @app.get("/potions")
 def get_potions():
-    return {"potions":potions_db}
+    potions = load_potions()
+    return {"potions":potions}
 
-@app.get("/potions/{potion_id}")
-def get_potion(potion_id:int):
-    potion = next((p for p in potions_db if p["id"]== potion_id), None)
-    if potion:
-        return potion
-    
-    return {"error": "Potion not found"}
+
+@app.post("/potions")
+def add_potion(potion:dict):
+    potions = load_potions()
+    potion["id"] = max((p["id"] for p in potions), default=0) + 1
+    potions.append(potion)
+    save_potions(potions)
+    return {"message": "Potion added!", "potions": potion}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8001)
--- inventory-service/app/tempdb.py
+++
@@ -1,6 +0,0 @@
-potions_db = [
-    {"id":1 , "name": "Healing Tonic", "price":50, "effect":"Restores Health"},
-    {"id":2 , "name": "Turbo Force", "price":150, "effect":"Grants Turbo boost"},
-    {"id":3 , "name": "Invisibility Shake", "price":40, "effect":"Grants Invisibility Cloak"}
-    
-]
