71f9e372a1d7a30b87580bd50e3205b098991b44
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2025-02-06 07:56:25 -0500

    folder reorganization

--- backend/store-service/app/main.py
+++ backend/store-service/app/main.py
@@ -9,3 +9,7 @@ INVENTORY_URL = "http://localhost:8001"
 def get_potions():
     return {"status":"OK"}
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8002)
+
